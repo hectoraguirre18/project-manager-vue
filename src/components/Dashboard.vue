4f60755440cf69517b05298255417581c8180dbd
Hector <a320793@uach.mx>
2020-11-26 23:48:41 -0700

    moved navbar into its own component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,32 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-
-        <b-nav-item-dropdown text="Lang" right>
-        <b-dropdown-item href="#">EN</b-dropdown-item>
-        <b-dropdown-item href="#">ES</b-dropdown-item>
-        <b-dropdown-item href="#">RU</b-dropdown-item>
-        <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-        <template #button-content>
-          <span>Account</span>
-        </template>
-        <b-dropdown-item href="#" @click="logout">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-
+    <Navbar/>
     <b-table id="projectsTable"
       striped hover
       :items="projects"
@@ -41,7 +15,13 @@ import dateFormat from 'dateformat';
 import axios from 'axios';
 import {mapActions} from 'vuex';
 
+import Navbar from './Navbar'
+
+
 export default {
+  components: {
+    Navbar
+  },
   data() {
     return {
       fields: [
